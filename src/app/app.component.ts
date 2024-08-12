d31fc588dc376c41b1295dae1d1b08f09642a5f9
stormpanda <maikthomalla@gmail.com>
2024-08-12 08:51:23 +0200

    feat: restructure examples a bit making them a dictionary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { BehaviorSubject, combineLatest, forkJoin, interval, map, Observable, Subject, Subscription, take, takeUntil, tap } from 'rxjs';
-import { AsyncPipe, NgIf } from '@angular/common';
+import { AsyncPipe, NgFor, NgIf } from '@angular/common';
 import { FakeConsoleComponent } from './components/fake-console/fake-console.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FakeConsoleComponent, AsyncPipe, NgIf],
+  imports: [RouterOutlet, FakeConsoleComponent, AsyncPipe, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -22,31 +22,43 @@ export class AppComponent {
     return tap((value: string) => this.log(`Source: ${value}`));
   }
 
-  sourceA$ = interval(1200).pipe(
-    map(value => `A: ${value}`), 
-    this.tapLogSource(),
-    takeUntil(this.completeSubject)
-  );
+  private readonly sources = {
+    sourceA$: interval(1200).pipe(
+      map(value => `A: ${value}`), 
+      this.tapLogSource(),
+      takeUntil(this.completeSubject)
+    ),
+  
+    sourceB$: interval(2200).pipe(
+      map(value => `B: ${value}`), 
+      this.tapLogSource(),
+      takeUntil(this.completeSubject)
+    ),
+  
+    sourceC$: interval(3200).pipe(
+      map(value => `C: ${value}`), 
+      this.tapLogSource(),
+      takeUntil(this.completeSubject)
+    )
+  } as const
 
-  sourceB$ = interval(2200).pipe(
-    map(value => `B: ${value}`), 
-    this.tapLogSource(),
-    takeUntil(this.completeSubject)
-  );
+  private readonly examples = {
+    combineLatest$: combineLatest([this.sources.sourceA$, this.sources.sourceB$, this.sources.sourceC$]).pipe(
+      map(([a, b, c]) => `${a} ${b} ${c}`)
+    ),
+  
+    forkJoin$: forkJoin([this.sources.sourceA$, this.sources.sourceB$, this.sources.sourceC$]).pipe(
+      map(([a, b, c]) => `${a} ${b} ${c}`)
+    )
+  } as const
 
-  sourceC$ = interval(3200).pipe(
-    map(value => `C: ${value}`), 
-    this.tapLogSource(),
-    takeUntil(this.completeSubject)
-  );
+  getSources() {
+    return Object.entries(this.sources).map(([key, value]) => ({ name: key, observable$: value }));
+  }
 
-  combineLatest$ = combineLatest([this.sourceA$, this.sourceB$, this.sourceC$]).pipe(
-    map(([a, b, c]) => `${a} ${b} ${c}`)
-  );
-
-  forkJoin$ = forkJoin([this.sourceA$, this.sourceB$, this.sourceC$]).pipe(
-    map(([a, b, c]) => `${a} ${b} ${c}`)
-  );
+  getExamples() {
+    return Object.entries(this.examples).map(([key, value]) => ({ name: key, observable$: value }));
+  }
 
   subscribe(observable: Observable<string>) {
     this.completeSources();
@@ -55,12 +67,19 @@ export class AppComponent {
     this.log('Subscribing...');
     this.log('---');
 
-    observable.pipe(
-      tap(value => {
+    observable.subscribe({
+      next: (value) => {
         this.log(`Subscription: ${value}`);
         this.log('---');
-      }),
-    ).subscribe();
+      },
+      complete: () => {
+        this.log('###');
+        this.log('Subscription completed');
+      },
+      error: (error) => {
+        this.log(`Subscription error: ${error}`);
+      }
+    });
   }
 
   completeSources() {
